7537ca971e31cbd9881217875f5b70d7aa2ee870
aabhas12 <aabhassi@buffalo.edu>
2018-08-16 01:39:09 -0400

    First file

--- recipe/tests.py
+++ recipe/tests.py
@@ -148,7 +148,7 @@ class RecipeTest(TestCase):
     def test_valid_delete_recipe(self):
         response = self.client.delete(
             reverse('recipe', kwargs={'pk': self.first_recipe.pk}))
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
 
 
